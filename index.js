a7ca895c47cd6abf8d4ce106156ed31965b445c6
HannanBhuiyan <hannan.diuh@gmail.com>
2023-03-23 00:23:00 +0600

    redux initial commit

--- index.js
+++ index.js
@@ -1,35 +1,62 @@
-// defined contant
 
+const { createStore } = require('redux')
+
+// define const
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
-const ADD_USER = 'ADD_USER';
 
-// State init
+// create state
 const initialCounterState = {
      count: 0
 }
 
-const initialUserState = () => {
-     user: [{userName: "Jhon"}]
-}
 
-// action
-const incrementCounter = () => {
+// create action
+const createIncrementAction = () => {
      return {
           type: INCREMENT
-     }
+     }    
 }
 
-const decrementCounter = () => {
+const createDecrementAction = () => {
      return {
-          type: DECREMENT
+          type : DECREMENT
      }
 }
 
+// create reducer
 
-const addUser = () => {
-     return {
-          type: ADD_USER,
-          payload: {userName: "jhon doe"}
+
+const counterReducer = (state=initialCounterState, action) => {
+     switch(action.type){
+          case INCREMENT:
+               return {
+                    ...state,
+                    count: state.count + 1
+               }
+          case DECREMENT:
+               return {
+                    ...state,
+                    count: state.count - 1
+               }
+          default:
+               return state;
      }
 }
+
+ 
+ 
+
+// create store
+
+let store = createStore(counterReducer)
+
+store.subscribe( () => console.log(store.getState()))
+
+store.dispatch(createIncrementAction())
+store.dispatch(createIncrementAction())
+store.dispatch(createIncrementAction())
+store.dispatch(createIncrementAction())
+store.dispatch(createIncrementAction())
+
+store.dispatch(createDecrementAction())
